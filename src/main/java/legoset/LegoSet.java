e6b9f7b1f9733d26c1a02b68f93d952aaf7c584d
Kirlos Yousef <kirlosek@gmail.com>
2020-04-23 16:43:49 +0200

    Added legoset

--- src/main/java/legoset/LegoSet.java
+++ src/main/java/legoset/LegoSet.java
@@ -1,4 +1,37 @@
 package legoset;
 
+import lombok.Data;
+import movie.YearAdapter;
+
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.Year;
+import java.util.List;
+import java.util.Set;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"name", "theme", "subtheme", "year", "pieces", "tags", "minifigs", "weight", "url"})
+@Data
 public class LegoSet {
+
+    @XmlJavaTypeAdapter(YearAdapter.class)
+    private Year year;
+
+    @XmlElementWrapper(name = "tags")
+    @XmlElement(name= "tag")
+    private Set<String> tags;
+
+    @XmlElementWrapper(name = "minifigs")
+    @XmlElement(name= "minifig")
+    private List<Minifig> minifigs;
+    private Weight weight;
+    private int pieces;
+    private String name;
+    private String theme;
+    private String subtheme;
+    private String url;
+
+    @XmlAttribute
+    private String number;
 }
--- src/main/java/legoset/Main.java
+++ src/main/java/legoset/Main.java
@@ -1,4 +1,37 @@
 package legoset;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Set;
+
+import jaxb.JAXBHelper;
+
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        LegoSet legoSet = new LegoSet();
+        legoSet.setNumber("75211");
+        legoSet.setName("Imperial TIE Fighter");
+        legoSet.setTheme("Star Wars");
+        legoSet.setSubtheme("Solo");
+        legoSet.setYear(Year.of(2018));
+        legoSet.setPieces(519);
+        legoSet.setTags(Set.of("Starfighter", "Stormtrooper", "Star Wars", "Solo"));
+        legoSet.setWeight(new Weight(0.89, "kg"));
+        legoSet.setUrl("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter");
+
+        ArrayList<Minifig> minifigs = new ArrayList<>();
+        minifigs.add(new Minifig("Imperial Mudtrooper", 2));
+        minifigs.add(new Minifig("Imperial Pilot", 1));
+        minifigs.add(new Minifig("Mimban Stormtrooper", 1));
+        legoSet.setMinifigs(minifigs);
+
+        JAXBHelper.toXML(legoSet, System.out);
+
+        JAXBHelper.toXML(legoSet, new FileOutputStream("legoSet.xml"));
+        System.out.println(JAXBHelper.fromXML(LegoSet.class, new FileInputStream("legoSet.xml")));
+    }
+
 }
